57f8aedc2e6ba3f4c3b081a2595fd68508472165
GC1920 <gcmenezes1@gmail.com>
2022-11-29 15:00:37 -0400

    Animated splash screen

--- App.js
+++ App.js
@@ -1,7 +1,28 @@
+import React, { useState } from 'react';
+import AnimatedSplash from 'react-native-animated-splash-screen';
+
 import { Routes } from "./src/routes/Index";
 
 export default function App() {
+
+  const [loading, setLoading] = useState(false);
+
+  setTimeout(() => {
+    setLoading(true);
+  }, 2000);
+
   return (
-      <Routes />
+      <AnimatedSplash
+        transludecent={true}
+        isLoaded={loading}
+        logoImage={require('./assets/android/drawable-hdpi/splash.png')}
+        backgroundColor={"#191919"}
+        logoHeight={800}
+        logoWidth={800}
+      >
+
+        <Routes />
+
+      </AnimatedSplash>
   );
 }
